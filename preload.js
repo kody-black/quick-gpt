fac236fa2f8acfbcde6285d56b75a5bd08461b07
kody <tupobwq@foxmail.com>
2024-07-28 00:29:28 +0800

    refactor: 修改有误的创建逻辑

--- preload.js
+++ preload.js
@@ -6,23 +6,6 @@ const CONFIG_TEMPLATE_PATH = path.join(__dirname, 'config.template.json');
 userDataPath = utools.getPath('userData');
 const CONFIG_PATH = path.join(userDataPath, 'quickgpt_config.json');
 
-// 检查如果没有配置文件则创建一个
-if (!fs.existsSync(CONFIG_PATH)) {
-    console.log('Creating config.json from template...');
-    fs.copyFileSync(CONFIG_TEMPLATE_PATH, CONFIG_PATH);
-    // 打开配置文件
-    window.openSetting();
-}
-
-utools.onPluginEnter(({ code }) => {
-    if (code === 'gptconfig') {
-        window.openSetting();
-    }
-    else if (global.CONFIG === undefined) {
-        window.loadConfig();
-    }
-});
-
 // 加载配置文件
 window.loadConfig = function () {
     global.CONFIG = JSON.parse(fs.readFileSync(CONFIG_PATH, 'utf-8'));
@@ -55,3 +38,22 @@ window.openSetting = function () {
     });
 
 }
+
+// 检查如果没有配置文件则创建一个
+if (!fs.existsSync(CONFIG_PATH)) {
+    console.log('Creating config.json from template...');
+    fs.copyFileSync(CONFIG_TEMPLATE_PATH, CONFIG_PATH);
+    // 打开配置文件
+    window.openSetting();
+}
+
+utools.onPluginEnter(({ code }) => {
+    if (code === 'gptconfig') {
+        window.openSetting();
+    }
+    else if (global.CONFIG === undefined) {
+        window.openSetting();
+    }
+});
+
+
